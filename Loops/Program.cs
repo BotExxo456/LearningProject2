93a41880180180ab0ec98ef3c6a0ad8b5b83870d
BotExxo456 <reinisch.lucas070300@gmail.com>
2024-04-18 20:33:56 +0200

    ForLoops

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -1,7 +1,21 @@
-﻿internal class Program
+﻿internal class Program1
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        for(int counter = 0; counter < 50; counter+=5)
+        {
+            Console.WriteLine(counter);
+        }
+        
+
+        for(int odd = 0; odd < 20; odd++)
+        {
+            if (odd % 2 != 0)
+            {
+                Console.WriteLine(odd);
+            }
+            
+        }  
+        Console.Read();
     }
 }
--- Loops/obj/Debug/net8.0/Loops.AssemblyInfo.cs
+++ Loops/obj/Debug/net8.0/Loops.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Loops")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d2a51b97866a343af0e8adb2ed1d87997bb8f50")]
 [assembly: System.Reflection.AssemblyProductAttribute("Loops")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Loops")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
